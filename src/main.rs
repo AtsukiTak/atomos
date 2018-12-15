b14d95aa7f15138110064637fb7c12fd0febf2e0
AtsukiTak <takatomgoo@gmail.com>
2018-12-16 00:45:01 +0900

    print Hello World! to VGA buffer

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,16 @@ use core::panic::PanicInfo;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in "Hello World!".iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
 
